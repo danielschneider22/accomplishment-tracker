a0e79a458169d388dbea805801022a980a0d1af8
danielschneider22 <danielschneider22@gmail.com>
2023-11-05 16:44:03 -0500

    navbar 2.0

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import Image from "next/image"
 import trophy from '@/public/images/trophy_no_bg.png'
+import styles from './NavBar.module.css'
 
 import React from 'react'
 
@@ -18,27 +19,24 @@ const NavBar = () => {
     });
 
     return (
-        <nav className="bg-gray-800">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <nav className={`bg-gray-800 ${styles.navBar}`}>
+        <div className="mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              {/* Mobile menu button */}
-              <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                <span className="absolute -inset-0.5"></span>
-                <span className="sr-only">Open main menu</span>
-                  {/* Icon when menu is closed.
-                  Menu open: "hidden", Menu closed: "block" */}
-                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-                  {/* Icon when menu is open.
-                  Menu open: "block", Menu closed: "hidden" */}
-                <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className={`sm:hidden`} id={styles.menuToggle}>
+                <input type="checkbox" />
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <div id={styles.menu} className="bg-gray-800">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+                        </div>
+                    </div>
+           </div>
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start w-full absolute sm:relative">
               <div className="flex flex-shrink-0 items-center">
                 <Image className="h-8 w-auto" src={trophy} alt="Your Company" />
               </div>
@@ -63,11 +61,15 @@ const NavBar = () => {
               {/* Profile dropdown */}
               <div className="relative ml-3">
                 <div>
-                  <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                  <div className="relative flex rounded-full bg-gray-800 cursor-pointer" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                     <span className="absolute -inset-1.5"></span>
                     <span className="sr-only">Open user menu</span>
-                    <Image className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" width={100} height={100} />
-                  </button>
+                    <div className="flex flex-col justify-center items-center">
+                        <Image className="h-8 w-8 rounded-full mb-1" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" width={100} height={100} />
+                        <span className="text-xs font-bold text-yellow-300">Exercise Guru</span>
+                    </div>
+                    
+                  </div>
                 </div>
                 <div className="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
                   <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
@@ -78,16 +80,7 @@ const NavBar = () => {
             </div>
           </div>
         </div>
-      
-        {/* Mobile menu, show/hide based on menu state.  */}
-        <div className="sm:hidden" id="mobile-menu">
-          <div className="space-y-1 px-2 pb-3 pt-2">
-            <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
-          </div>
-        </div>
+        
       </nav>
     )
 }
