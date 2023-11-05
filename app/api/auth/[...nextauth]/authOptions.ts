367f100503b0f3d867e9ecbb9c25d7bb1b8e6ec8
danielschneider22 <danielschneider22@gmail.com>
2023-11-05 14:41:15 -0500

    llama

---
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,41 @@
+import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import GitHubProvider from "next-auth/providers/github";
+import bcrypt from 'bcrypt'
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {label: 'Email', type: 'email', placeholder: 'asdf@gmail.com'},
+                password: {label: 'Password', type: 'password'}
+            },
+            async authorize(credentials, req) {
+                if(!credentials?.email || !credentials?.password) return null;
+
+                const user = await prisma.user.findUnique({ where: { email: credentials.email}})
+                if(!user) return null;
+
+                const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword!);
+
+                return passwordsMatch ? user : null;
+            }
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!
+        })
+    ],
+    session: {
+        strategy: "jwt"
+    }
+}
