4e969c165c23bc7262445ca2dad545617f3ab811
danielschneider22 <danielschneider22@gmail.com>
2023-11-05 19:17:45 -0500

    major step

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/authOptions';
+import ListCard from './components/ListCard/ListCard';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-  console.log(session)
   
   return (
     <main>
-      <h1 className={"font-poppins"}>Hello {session && <span>{session.user!.name}</span>}</h1>
+      {/* <h1 className={"font-poppins"}>Hello {session && <span>{session.user!.name}</span>}</h1> */}
+      <div className="flex p-5 sm:gap-x-4 gap-y-5 flex-col sm:flex-row">
+        <ListCard title={"General"} />
+        <ListCard title={"Exercise"} />
+        <ListCard title={"Music"} />
+      </div>
+      
+      <button className={"bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full"}>Complete Day!</button>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      minHeight: {
+        '5': '1.25rem',
+        '10': '2.5rem',
+      }
     },
   },
   plugins: [],
